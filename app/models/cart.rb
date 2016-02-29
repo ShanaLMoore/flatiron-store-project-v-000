35733802a1d0192c199f74b2f7b3d02f740143ba
Shana L Moore <shana.lavina.moore@gmail.com>
2016-02-29 11:43:49 -0800

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,14 +2,17 @@ class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   has_many :items, through: :line_items
   belongs_to :user
+  
+  def total
+    line_items.collect {|i| i.item.price * i.quantity}.inject(:+)
+  end
 
   def add_item(item_id)
-    line_item = self.line_items.find_by(item_id: item_id) 
-    if line_item 
-      line_item.quantity += 1
-    else
-      line_item=self.line_items.build(item_id: item_id)
-    end
+    item = Item.find(item_id)
+    line_item = line_items.detect {|li| li.item == item}
+    line_item ||= LineItem.new
+    line_item.cart = self
+    line_item.item = item
     line_item
   end
 
