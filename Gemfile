5585f2816ec58bda90addf904fdabf268d4764d8
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-03 09:59:01 -0800

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem'devise'
+# gem'devise'
 gem 'bcrypt'
 
 gem 'rails', '4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160302041055) do
 
   create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -54,10 +55,9 @@ ActiveRecord::Schema.define(version: 20160302041055) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.integer  "current_cart_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "integer"
+    t.integer  "current_cart_id"
   end
 
 end
