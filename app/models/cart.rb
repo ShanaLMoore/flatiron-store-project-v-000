a23deb02ddf374e1a4b8db63ede5625c90071ceb
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-04 15:56:38 -0800

    initial commit

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -19,6 +19,15 @@ class Cart < ActiveRecord::Base
 
   def checkout
     self.status = "submitted"
+    change_inventory
   end
 
+  def change_inventory
+    if self.status = "submitted"
+      self.line_items.each do |line_item| 
+        line_item.item.inventory -= line_item.quantity
+        line_item.item.save
+      end
+    end 
+  end
 end
